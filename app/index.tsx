8a2d730a8cfe6743756cd27651f4860d761ee22c
FabY34 <fabianoluque88@gmail.com>
2025-05-21 15:40:27 +0000

    velha grossa

---
+++ app/index.tsx
@@ -0,0 +1,49 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function index() {
+  const nome = "Fabiano";
+
+  return (
+  <View>
+   <Text>Jogo da Velha</Text>
+   <Text>Vencedor</Text>
+   
+   <View>
+     <View>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+     </View>
+     <View>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+     </View>
+     <View>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+     </View>
+   </View>
+
+  </View>
+  );
+}
